a1f6b482bd6f7ee538b9ed20b9dd9f5e5b37feb0
pixiepucker <autumnoffic@gmail.com>
2022-04-23 21:56:50 -0500

    finished post route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const { v4: uuidv4 } = require('uuid');
 //require json from db
 const { db } = require('./db/db.json');
 
@@ -21,9 +23,20 @@ app.get('/api/notes', (req,res) => {
 
 //use post to create new note
 app.post('/api/notes', (req,res) => {
-    //check incoming content
-    console.log(req.body);
-    res.json(req.body);
+    //parse incoming data
+    const notesData = JSON.parse(fs.readFileSync('./db/db.json'));
+    //add unique ID to notes
+    req.body.id = notesData.length.toString();
+    //add note to json/arr
+    const newNote = req.body;
+    notesData.push(newNote);
+    //write new note to db/db.json
+    fs.writeFileSync(
+        './db/db.json',
+        JSON.stringify(notesData)
+    );
+    //send back response
+    res.json(notesData);
 });
 
 //server listen
