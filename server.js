868239b3f75b9d9e5bec6b256344c87a4ac16f8f
pixiepucker <autumnoffic@gmail.com>
2022-04-23 21:16:29 -0500

    added middleware and post route

--- server.js
+++ server.js
@@ -5,10 +5,25 @@ const app = express();
 //require json from db
 const { db } = require('./db/db.json');
 
+//add middleware here
+//parse incoming string/arr data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+
 //write get routes here
+
+//get notes
 app.get('/api/notes', (req,res) => {
     let results = db;
     res.json(results);
+});
+
+//use post to create new note
+app.post('/api/notes', (req,res) => {
+    //check incoming content
+    console.log(req.body);
+    res.json(req.body);
 });
 
 //server listen
